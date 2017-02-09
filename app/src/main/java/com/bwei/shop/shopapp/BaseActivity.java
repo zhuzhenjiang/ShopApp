c031d614a432f50e2813b34b389b1aeee9e18e79
zhuzhenjiang <Zzjdsy@Gmail.com>
2017-02-09 10:38:59 +0800

    基类

---
+++ app/src/main/java/com/bwei/shop/shopapp/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.bwei.shop.shopapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/2/9 0009.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    
+    
+    
+}
